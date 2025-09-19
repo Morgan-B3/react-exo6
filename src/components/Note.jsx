d9174ffa61b285565550bc7144528efc3dc48190
Morgan-B3 <boutroism@gmail.com>
2025-09-19 16:45:16 +0200

    not finished

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -9,7 +9,7 @@ function Note({note, actionDelete}) {
         <h4>{note.title}</h4>
         <p>{note.content}</p>
         <p>{note.createdAt}</p>
-        <button onClick={()=>navigate(`notes/${note.id}/update`)}>Modifier</button>
+        <button onClick={()=>navigate(`/notes/${note.id}/update`)}>Modifier</button>
         <button onClick={()=>actionDelete()}>Supprimer</button>
     </li>
   )
